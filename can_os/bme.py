0c56bc0e85860fa45a092386871161f0bc276619
Mikołaj Giza <93527772+gmikx@users.noreply.github.com>
2023-03-01 12:21:39 +0100

    Changing from procedural to OO

--- can_os/bme.py
+++ can_os/bme.py
@@ -1,6 +1,15 @@
 from bme280pi import Sensor
+import time
 
-def zbierz_dane_bme():
-    sensor = Sensor()
-    dane = sensor.get_data()
-    return dane
+class BME():
+    def __init__(self) -> None:
+        self.sensor = Sensor()
+
+    def zbierz_dane(self):
+        dane = self.sensor.get_data()
+        return dane
+
+    # def zbieraj_dane(self, opoznienie):
+    #     self.zbierz_dane_bme()
+    #     time.sleep(opoznienie)
+    #     return
